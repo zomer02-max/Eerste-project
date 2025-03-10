1b269386c7f806497773f9abef2fcc6ef57b93d9
Remco van Zomeren <zomer02@MacBook-van-Remco.local>
2025-03-10 14:56:18 +0100

    Toegevoegd: programma voor het optellen van twee getallen

--- script.py
+++ script.py
@@ -1,1 +1,7 @@
-print("Hallo, wereld! Mijn eerste GitHub-project in Cursor!")
+def som(a, b):
+    return a + b
+
+getal1 = int(input("Geef het eerste getal: "))
+getal2 = int(input("Geef het tweede getal: "))
+
+print(f"De som is: {som(getal1, getal2)}")
